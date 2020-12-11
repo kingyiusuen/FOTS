984041653a1f6e7b382c95ffa189472101cabd8c
kingyiusuen <kingyiusuen@gmail.com>
2020-12-11 11:31:35 -0600

    complete recognition loss

--- loss.py
+++ loss.py
@@ -4,14 +4,16 @@ import torch.nn.functional as F
 from torch.nn import CTCLoss
 
 class DetectionLoss(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, lambda_reg=1, lambda_angle=10):
         super().__init__()
+        self.lambda_reg = lambda_reg
+        self.lambda_angle = lambda_angle
 
     def forward(self, score_map_true, geo_map_true, angle_map_true, 
-            score_map_pred, geo_map_pred, angle_map_pred, training_mask, lambda_reg=1):
+            score_map_pred, geo_map_pred, angle_map_pred, training_mask):
         L_cls = self.cls_loss(score_map_true, score_map_pred, training_mask)
         L_reg = self.reg_loss(geo_map_true, geo_map_pred, angle_map_true, angle_map_pred)
-        L_detect = L_cls + lambda_reg * L_reg
+        L_detect = L_cls + self.lambda_reg * L_reg
         return L_detect
 
     def cls_loss(self, score_map_true, score_map_pred, training_mask):
@@ -24,11 +26,11 @@ class DetectionLoss(torch.nn.Module):
         L_cls = torch.sum(L_cls * training_mask) / torch.sum(training_mask)
         return L_cls
 
-    def reg_loss(self, geo_map_true, geo_map_pred, angle_map_true, angle_map_pred, lambda_angle=10):
+    def reg_loss(self, geo_map_true, geo_map_pred, angle_map_true, angle_map_pred):
         """ Regression loss. """
         L_iou = self.iou_loss(geo_map_true, geo_map_pred)
         L_angle = torch.cos(angle_map_pred - angle_map_true)
-        L_reg = L_iou + lambda_angle * (1 - L_angle)
+        L_reg = L_iou + self.lambda_angle * (1 - L_angle)
         return torch.mean(L_reg)
 
     def iou_loss(self, geo_map_true, geo_map_pred, smooth=1):
@@ -50,16 +52,27 @@ class DetectionLoss(torch.nn.Module):
 class RecognitionLoss(nn.Module):
     def __init__(self):
         super().__init__()
-        self.ctc_loss = CTCLoss()
+        self.ctc_loss = CTCLoss(blank=0, reduction='mean')
 
-    def forward(self):
-        pass
+    def forward(self, log_probs, indexed_tokens_true, len_pred, len_true):
+        L_recog = self.ctc_loss(log_probs, indexed_tokens_true, len_pred, len_true)
+        return L_recog
 
 class FOTSLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, lambda_recog=1):
         super().__init__()
         self.detection_loss = DetectionLoss()
         self.recognition_loss = RecognitionLoss()
+        self.lambda_recog = lambda_recog
 
-    def forward(self):
-        pass
+    def forward(self, score_map_true, geo_map_true, angle_map_true, 
+            score_map_pred, geo_map_pred, angle_map_pred, training_mask, 
+            log_probs, indexed_tokens_true, len_pred, len_true):
+        L_detect = self.detection_loss(
+            score_map_true, geo_map_true, angle_map_true, 
+            score_map_pred, geo_map_pred, angle_map_pred, 
+            training_mask
+        )
+        L_recog = self.recognition_loss(log_probs, indexed_tokens_true, len_pred, len_true)
+        L = L_detect + self.lambda_recog * L_recog
+        return L_detect, L_recog, L
