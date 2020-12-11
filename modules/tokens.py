fd75790218455aedd9b0ac3b65a86183a052be78
kingyiusuen <kingyiusuen@gmail.com>
2020-12-11 04:04:08 -0600

    add comment

--- modules/tokens.py
+++ modules/tokens.py
@@ -1,2 +1,2 @@
-TOKENS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-"
+TOKENS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-" # "-" is for unknown tokens
 NUM_OF_CLASSES = len(TOKENS) + 1 # add one for the blank token
